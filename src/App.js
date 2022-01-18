f11ee85088f4ffaee6277d85984a72694c43e849
Mark Clayton <mark.clayton@impervious.ai>
2022-01-18 16:56:27 -0600

    finally instantiated the wasm from buffer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as ldk from "lightningdevkit";
+import { ConfirmationTarget, FeeEstimator } from "lightningdevkit";
+
+const initializeFeeEstimator = () => {
+  let feeEstimator = FeeEstimator.new_impl(new FeeEstimator());
+  console.log(feeEstimator);
+};
+
+const compileWasm = (pathToWasm) => {
+  fetch(pathToWasm)
+    .then((response) => {
+      return response.arrayBuffer();
+    })
+    .then((bytes) => {
+      ldk
+        .initializeWasmFromBinary(bytes)
+        .then(() => initializeFeeEstimator())
+        .catch((err) => console.error(err));
+    });
+};
 
 const App = () => {
-  ldk.initializeWasmFromBinary(
-    "node_modules/lightningdevkit/liblightningjs.wasm"
-  );
+  useEffect(async () => {
+    compileWasm("liblightningjs.wasm");
+  });
+
   return (
     <div>
       <h1>Hello World</h1>
